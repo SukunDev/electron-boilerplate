d64038364f6293026985c5e2f28f57eccfcdd47c
Sukun Dev <sukundev32@gmail.com>
2025-12-10 21:47:39 +0700

    refactor: simplify layout structure in main route component

--- src/renderer/src/routes/_main.tsx
+++ src/renderer/src/routes/_main.tsx
@@ -15,11 +15,9 @@ function RouteComponent(): React.JSX.Element {
       <SidebarInset>
         <div className="flex flex-col h-screen">
           <AppHeader />
-          <main className="flex-1 overflow-y-auto">
-            <div className="bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
-              <div className="max-w-7xl mx-auto p-6">
-                <Outlet />
-              </div>
+          <main className="flex-1 overflow-y-auto bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
+            <div className="max-w-7xl mx-auto p-6">
+              <Outlet />
             </div>
           </main>
         </div>
--- src/renderer/src/routes/_main/index.tsx
+++ src/renderer/src/routes/_main/index.tsx
@@ -1,7 +1,7 @@
 import { useGetTodos } from '@renderer/api/getTodos'
 import { Button } from '@renderer/components/ui/button'
 import { Item, ItemActions, ItemContent, ItemMedia, ItemTitle } from '@renderer/components/ui/item'
-import { createFileRoute, Link, useNavigate } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 import { CheckCircle, ExternalLink } from 'lucide-react'
 import React from 'react'
 
@@ -11,48 +11,53 @@ export const Route = createFileRoute('/_main/')({
 
 function RouteComponent(): React.JSX.Element {
   const { data: todos, isLoading: fetchTodosLoading } = useGetTodos()
-  const navigate = useNavigate()
-
-  const handleLogout = async (): Promise<void> => {
-    navigate({to: '/signin'})
-  }
 
   return (
-    <div className="p-2 space-y-4">
-      <Button onClick={handleLogout}>Logout</Button>
-      <div className="space-y-4">
-        {fetchTodosLoading ? (
-          <p>Loading...</p>
-        ) : todos && todos.length > 0 ? (
-          todos.map((todo) => (
-            <Item key={todo.id} variant="outline" className='bg-white'>
-              <ItemMedia>
-                <CheckCircle className="size-4 text-green-500" />
-              </ItemMedia>
+    <>
+      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+          <div className="aspect-video rounded-base bg-background border-2 border-border" />
+          <div className="aspect-video rounded-base bg-background border-2 border-border" />
+          <div className="aspect-video rounded-base bg-background border-2 border-border" />
+        </div>
+        <div className="h-full flex-1 rounded-base bg-background border-2 border-border">
+          <div className="p-2 space-y-4">
+            <div className="space-y-4">
+              {fetchTodosLoading ? (
+                <p>Loading...</p>
+              ) : todos && todos.length > 0 ? (
+                todos.map((todo) => (
+                  <Item key={todo.id} variant="outline" className="bg-white">
+                    <ItemMedia>
+                      <CheckCircle className="size-4 text-green-500" />
+                    </ItemMedia>
 
-              <ItemContent>
-                <ItemTitle>{todo.title}</ItemTitle>
-              </ItemContent>
+                    <ItemContent>
+                      <ItemTitle>{todo.title}</ItemTitle>
+                    </ItemContent>
 
-              <ItemActions>
-                <Button
-                // onClick={() => completeTodoMutation(id)}
-                // disabled={completeTodoLoading}
-                >
-                  Complete
-                </Button>
-                <Button variant="noShadow" asChild>
-                  <Link to={`/`}>
-                    View Details <ExternalLink />
-                  </Link>
-                </Button>
-              </ItemActions>
-            </Item>
-          ))
-        ) : (
-          <p>No todos found</p>
-        )}
+                    <ItemActions>
+                      <Button
+                      // onClick={() => completeTodoMutation(id)}
+                      // disabled={completeTodoLoading}
+                      >
+                        Complete
+                      </Button>
+                      <Button variant="noShadow" asChild>
+                        <Link to={`/`}>
+                          View Details <ExternalLink />
+                        </Link>
+                      </Button>
+                    </ItemActions>
+                  </Item>
+                ))
+              ) : (
+                <p>No todos found</p>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
